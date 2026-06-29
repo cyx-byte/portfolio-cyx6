4e099d013b1ac72a5fc1c0439e742e696de24a9b
cyx-byte <245004541@qq.com>
2026-06-29 20:30:18 +0800

    proxy uploads to COS

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   trailingSlash: true,
   images: { unoptimized: true },
+  async rewrites() {
+    return [
+      {
+        source: '/uploads/:path*',
+        destination: 'https://portfolio-cyx6-1446625869.cos-website.ap-guangzhou.myqcloud.com/uploads/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
